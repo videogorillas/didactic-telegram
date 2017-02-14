e7d9ab87b5b597057bf1cc23b4e9ebca6aa7fa46
day <marina.rusakova@gmail.com>
2017-02-14 18:56:18 +0200

    fixed categories list

--- DidacticTelegram/app/product-utils.js
+++ DidacticTelegram/app/product-utils.js
@@ -1,7 +1,26 @@
 function getCategories(products) {
   // TODO: distinct, sort
-  products.forEach((item, i, arr) => { item.id = i });
-  return products;
+  let distinct = products.filter((p, i) => {
+    return products.indexOf(p) === i
+  })
+  
+  let cats = [];
+  products.forEach((product, i, arr) => {
+    let cat = cats.find(cat => cat.cat == product.cat);
+    if (!cat)
+      cats.push({cat: product.cat, count: 1});
+    else
+      cat.count++;
+  });
+  cats.sort((a, b) => {return b.count - a.count});
+  
+//   let cats = Array.from(products);
+  cats.forEach((cat, i, arr) => { 
+    cat.id = i;
+    cat.selected = true;
+  });
+  console.log('>> cats: ' + JSON.stringify(cats));
+  return cats;
 }
 
 export {getCategories}
--- DidacticTelegram/app/sell-component.js
+++ DidacticTelegram/app/sell-component.js
@@ -1,20 +1,27 @@
 import React, { Component } from 'react';
-import { ScrollView, View, Text, ListView, Image, Switch } from 'react-native';
+import { ScrollView, View, Text, ListView, Image, Switch, TouchableHighlight } from 'react-native';
 import { Actions } from 'react-native-router-flux';
 import { getCategories } from './product-utils'
+import {styles} from './styles'
 
 export default class SellComponent extends Component {
   constructor(props) {
       super(props);
       this.state = {cats: []};
   }
+  
+  componentWillMount() {
+    let cats = getCategories(this.props.products);
+    this.setState(prevState => ({
+      cats: cats
+    }));
+  }
 
   render() {
     let uri = this.props.photo;
-    let categories = getCategories(this.props.products);
-        
+
     const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});  
-    let dataSource = ds.cloneWithRows(categories);
+    let dataSource = ds.cloneWithRows(this.state.cats);
     
     return (
       <View style={{flex: 1}}>
@@ -23,23 +30,39 @@ export default class SellComponent extends Component {
             <Image source={{uri: uri}} resizeMode='cover' style={{flex: 1}}/>
           </View>
           <ListView dataSource={dataSource} renderRow={this.renderRow.bind(this)}/>
+          
+          <View style={{justifyContent: 'space-around', flexDirection: 'row', backgroundColor: 'black'}}>
+            <TouchableHighlight onPress={this.done.bind(this)} activeOpacity={1} underlayColor={'#d3d3d355'} style={{borderRadius: 48}}>
+              <Image source={require('./ic_forward_white_24dp.png')} style={styles.ibutton} />
+            </TouchableHighlight>
+          </View>
+          
         </ScrollView>
       </View>
     )
   }
   
-  renderRow(row) {
-      return (<View key={row.id} style={{backgroundColor: 'white', margin: 10, flexDirection: 'row'}}>
-        <Switch style={{marginBottom: 10}} value={this.state.cats[row.cat]} onValueChange={(value) => this.checkCat(row.cat, value)}/>
-        <Text style={{color: 'black', textAlign: 'left', alignSelf: 'center'}}>{row.cat}</Text>
-      </View>)
-   }
+  done() {
+     console.log('>> cats selected: ' + JSON.stringify(this.state.cats));
+  }
   
+  renderRow(cat, sectionID, rowID) {
+      return (
+      <View key={cat.id} style={{backgroundColor: 'white', margin: 10, flexDirection: 'row'}}>
+        <Switch style={{marginBottom: 10}} value={this.selected(cat)} onValueChange={(value) => this.checkCat.call(this, cat, value)}/>
+        <Text style={{color: 'black', textAlign: 'left', alignSelf: 'center'}}>{cat.cat.split('_').join(' ')}</Text>
+      </View>)
+  }
+  
+  selected(cat) {
+    return this.state.cats.find(c => c.cat == cat.cat).selected;
+  }
+ 
    checkCat(cat, checked) {
-     console.log('>> ' + cat + ' ' + checked + ' cats: ' + this.state.cats);
-     let c = this.state.cats;
-     c[cat] = checked;
-     this.setState({cats: c});
-     ;
+     let cats = Array.from(this.state.cats);
+     cats.find(c => c.cat == cat.cat).selected = checked;
+     this.setState(prevState => ({
+       cats: cats
+     }));
    }
 }
